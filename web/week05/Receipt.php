f0384bbd52087196f6a00c51caa7c71ddc24a68d
lethees <514180089@qq.com>
2020-02-24 02:16:53 -0700

    updates

--- web/week05/Receipt.php
+++ web/week05/Receipt.php
@@ -13,9 +13,6 @@ $db = get_db();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="week05.js"></script>
   </head>
   <body>    
@@ -182,7 +179,9 @@ echo "</tr>
 </div>
 </div>
 <br>
-             <button class="btn btn-primary hidden-print text-align:center" onclick="myFunction()"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Print</button>
+<div class="print">
+             <button class="btn btn-primary hidden-print" onclick="myFunction()"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Print</button>
+	    </div>
 </div>
 
     
