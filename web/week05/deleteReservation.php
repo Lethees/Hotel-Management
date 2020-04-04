9faa20638371854d4f3169763b617a3023907d7d
lethees <514180089@qq.com>
2020-04-04 05:03:33 -0600

    updates

--- web/week05/deleteReservation.php
+++ web/week05/deleteReservation.php
@@ -2,8 +2,15 @@
 $reservation_id = $_GET['id'];
 require "connection.php";
 $db = get_db();
-$delete = $db->prepare("DELETE FROM reservation WHERE id = $reservation_id");
-$delete->execute();
+try{
+    $delete = $db->prepare("DELETE FROM reservation WHERE id = $reservation_id");
+    $delete->execute();
+}
+catch (Exception $ex){
+    echo "Error with DB. Details: $ex";
+	die();  
+}
+header("Location: results3.php");
+ 
 die();
-header("Location: results3.php"); 
 ?>
--- web/week05/management.php
+++ web/week05/management.php
@@ -1,5 +1,5 @@
 <?php
-  $actions = array("Guest List", "Room List", "Search Guest", "New Guest", "Reservation", "New Parking", "Print Receipt", "Update & Delete");
+  $actions = array("Guest List", "Room List", "Search Guest", "New Guest", "Reservation", "New Parking", "Print Receipt", "Update & Del");
   $image = array("1.png", "2.png", "3.png", "4.png", "5.jpg", "6.png", "7.png", "8.png");
   $button = array("dark", "info", "success", "primary", "secondary", "warning", "success", "primary");
   $web = array("guestList.php", "roomList.php", "search.php", "Register.php", "reservation.php", "parking.php", "search2.php", "search3.php");
