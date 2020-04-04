70ed59ca4a23b79cdb63b699fba2c95854501316
lethees <514180089@qq.com>
2020-04-04 06:35:02 -0600

    updates

--- web/week05/update.php
+++ web/week05/update.php
@@ -9,12 +9,8 @@ $db = get_db();
 
 try
 {
-	$update = $db->prepare("UPDATE reservation SET room_number = '$RoomNumber', check_in_date = '$checkInDate', check_out_date = '$checkOutDate' WHERE id= $id");
+	$update = $db->prepare("UPDATE reservation SET room_number = '$RoomNumber', check_in_date = '$checkInDate', check_out_date = '$checkOutDate' WHERE id = $id");
 	$update->execute();
-	
-	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
-	// get id of last inserted row - save in $userId
-	// $userId = $db->lastInsertId("reservation_id_seq");
 }
 catch (Exception $ex)
 {
