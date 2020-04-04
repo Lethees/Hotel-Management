53a9c47aeaa22463bf4795d2b09b2fddfe45757f
lethees <514180089@qq.com>
2020-04-04 03:19:33 -0600

    updates

--- web/week05/results2.php
+++ web/week05/results2.php
@@ -41,7 +41,6 @@ $first_name = $_POST['FirstName'];
   <table class="table table-dark table-hover">
     <thead>
       <tr>
-        <th>Reservation ID</th>
         <th>First Name</th>
         <th>Last Name</th>
         <th>Phone Number</th>
@@ -57,15 +56,13 @@ $first_name = $_POST['FirstName'];
    require "connection.php";
    $db = get_db();
    if ($first_name != "" && $last_name != "" && $phone != "" && $first_name != null && $last_name != null && $phone != null){
-    foreach ($db->query("SELECT r.id as rid, r.check_in_date as check_in_date, r.check_out_date as check_out_date, r.room_number as room_number FROM reservation r INNER JOIN customer c ON r.customer_id = c.id WHERE c.last_name = '$last_name' AND c.phone = '$phone' AND c.first_name = '$first_name'") as $fRow)
+    foreach ($db->query("SELECT r.check_in_date as check_in_date, r.check_out_date as check_out_date, r.room_number as room_number FROM reservation r INNER JOIN customer c ON r.customer_id = c.id WHERE c.last_name = '$last_name' AND c.phone = '$phone' AND c.first_name = '$first_name'") as $fRow)
     {   
-       $rId = $fRow["rid"];
        $checkIn = $fRow["check_in_date"];
        $checkOut = $fRow["check_out_date"];
        $roomNumber = $fRow["room_number"];
  
      echo "<tr>";
-     echo "<td>$rId</td>";
      echo "<td>$first_name</td>";
      echo "<td>$last_name</td>";
      echo "<td>$phone</td>";
@@ -78,7 +75,7 @@ $first_name = $_POST['FirstName'];
    }
    else{
    foreach ($db->query("SELECT (c.id as cid, c.first_name as first_name , c.last_name as last_name, c.phone 
-   as phone, r.id as rid, r.check_in_date as check_in_date, r.check_out_date as check_out_date, r.room_number 
+   as phone, r.check_in_date as check_in_date, r.check_out_date as check_out_date, r.room_number 
    as room_number) FROM customer c INNER JOIN reservation r ON c.id = r.customer_id 
    WHERE c.last_name = '$last_name' OR c.phone = '$phone' OR c.first_name = '$first_name'") as $fRow)
    { 
@@ -86,7 +83,6 @@ $first_name = $_POST['FirstName'];
     $last_name = $fRow["last_name"];
       $phone = $fRow["phone"];
 
-      $rId = $fRow["rid"];
       $checkIn = $fRow["check_in_date"];
       $checkOut = $fRow["check_out_date"];
       $roomNumber = $fRow["room_number"];
