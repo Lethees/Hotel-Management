dd466440f260c1da26230e16fd204b5f518d3022
lethees <514180089@qq.com>
2020-04-03 23:49:43 -0600

    updates

--- web/week05/signIn.php
+++ web/week05/signIn.php
@@ -51,79 +51,57 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
 // If we get to this point without having redirected, then it means they
 // should just see the login form.
 ?>
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Sign in</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="week05.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  </head>
-  <body>    
-    <div class="jumbotron text-center">
-        <h1>Welcome to Pines Inn, Annandale, VA</h1>
-        <p>Sign in to Access the Admin Panel</p> 
-    </div>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-end">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Pines Inn</a>
-            </li>
-        </ul>
-        <form class="form-inline" action="">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-success" type="submit">Search</button>
-        </form>
-    </nav>
-    <br>
-	<div class="container">
-<?php
+<body>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
+			</div>
+			<div class="card-body">
+			<form id="mainForm" action="signIn.php" method="POST">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" name="txtUser" placeholder="username">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" name="txtPassword" placeholder="password">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+					</div>
+					<?php
 if ($badLogin)
 {
 	echo "Incorrect username or password!<br /><br />\n";
 }
 ?>
-
-<h1>Please sign in below:</h1>
-
-<form id="mainForm" action="signIn.php" method="POST" class="was-validated">
-	<div class="form-group form-check">
-	<label class="form-check-label" for="txtUser">Username</label>
-		<input type="text" id="txtUser" class="form-control" name="txtUser" placeholder="Username" required>
-		<div class="valid-feedback">Valid.</div>
-    	<div class="invalid-feedback">Username can't be blank!</div>
+					<div class="form-group">
+						<input type="submit" value="Sign in" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="signUp.php">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
 	</div>
-
-	<div class="form-group form-check">
-		<label class="form-check-label" for="txtPassword">Password</label>
-		<input type="password" id="txtPassword" class="form-control" name="txtPassword" placeholder="Password" required>
-		<div class="valid-feedback">Valid.</div>
-   	 	<div class="invalid-feedback">Password can't be blank!</div>
-	</div>
-
-	<div class="form-group" style="float: right;">
-	<label class="form-check-label">
-      <input class="form-check-input" type="checkbox"> Remember me
-    </label>
-	</div>
-	<div class="form-group" style="float: right;">
-	<button type="submit" class="btn btn-primary">Sign In</button>
-	</div>
-</form>
-
-<br />
-<div style="float: right;">
-Don't have an account yet? <a href="signUp.php">Sign up</a> for a new account.
 </div>
-<br />
-
-</div>
-<?
-	require("footer.php");
-?>
 </body>
 </html>
